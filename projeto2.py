97053e0d57f513616b02d1ee9d3ad9b575a1b0aa
GabrielCruzT <53840235+GabrielCruzT@users.noreply.github.com>
2023-09-27 11:11:36 -0300

    Atividade 3
    Realização da atividade 3

--- projeto2.py
+++ projeto2.py
@@ -1,9 +1,12 @@
+
+from datetime import datetime
 class Pessoa:
-    def __init__(self,nome,idade,altura,peso):
+    def __init__(self,nome,idade,altura,peso,nascimento):
         self.nome = nome
         self.idade = idade
         self.altura = altura
         self.peso = peso
+        self.nascimento = nascimento
 
     def eh_maior(self):
         if self.idade>=18:
@@ -30,8 +33,31 @@ class Pessoa:
         else:
             self.imc_longo = "Obesidade grau III"
         return  self.imc_longo
-            
 
-pessoa1 = Pessoa("Zezinho", 19, 1.53, 68.2)
+while True:
+    nome_pessoa = input('Digite seu nome: ')
+    idade_pessoa = input('Digite sua idade: ')
+    altura_pessoa = input('Digite sua altura: ')
+    peso_pessoa = input('Digite seu peso: ')
+    nascimento_pessoa = input('Digite sua data de nascimento (formato dd/mm/aaaa): ')
+    data_formato = "%d/%m/%Y"
+    res = True
+    try:
+        res = bool(datetime.strptime(nascimento_pessoa, data_formato))
+        altura_pessoa = float(altura_pessoa)
+        peso_pessoa = float(peso_pessoa)
+        idade_pessoa = int (idade_pessoa)
+    except ValueError:
+        res = False
+
+    if all(c.isalpha() or c.isspace() for c in nome_pessoa) and idade_pessoa>0 and altura_pessoa>0 and peso_pessoa>0 and res==True:
+        print("ok")
+        break
+    else:
+        print("Dados errados")         
+
+pessoa1 = Pessoa(nome_pessoa, idade_pessoa, altura_pessoa, peso_pessoa, nascimento_pessoa)
+
+print(pessoa1.nome)
 print(pessoa1.imc_longo()) 
 print(pessoa1.eh_maior())
